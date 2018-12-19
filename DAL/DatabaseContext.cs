d94fda2108f9701794b0980e42e7380c17ce67a5
DESKTOP-AV9NNBP\MijalH <kyrcooler@gmail.com>
2018-12-19 17:25:40 +0100

    Added sample classess to database

--- DAL/DatabaseContext.cs
+++ DAL/DatabaseContext.cs
@@ -11,7 +11,25 @@ namespace DAL
             : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
+            modelBuilder.Entity<SchoolClass>().HasData(
+                new SchoolClass() {Id= 100, Name = "1A"},
+                new SchoolClass() {Id= 101,Name = "1B"},
+                new SchoolClass() {Id= 102,Name = "1C"},
+                new SchoolClass() {Id= 103,Name = "1D"},
+                new SchoolClass() {Id= 104,Name = "2A"},
+                new SchoolClass() {Id= 105,Name = "2B"},
+                new SchoolClass() {Id= 106,Name = "2C"},
+                new SchoolClass() {Id= 107,Name = "3A"},
+                new SchoolClass() {Id= 108,Name = "3B"},
+                new SchoolClass() {Id= 109,Name = "3C"},
+                new SchoolClass() {Id= 110,Name = "3S"},
+                new SchoolClass() {Id= 111,Name = "4A"},
+                new SchoolClass() {Id= 112,Name = "4B"});
+        }
         public DbSet<Mark> Marks { get; set; }
         public DbSet<Message> Messages { get; set; }
         public DbSet<ParentAccount> ParentAccounts { get; set; }
---
+++ DAL/Migrations/20181219162439_schol classess.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class scholclassess : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "SchoolClasses",
+                columns: new[] { "Id", "Name", "TeacherAccountId" },
+                values: new object[,]
+                {
+                    { 100, "1A", null },
+                    { 101, "1B", null },
+                    { 102, "1C", null },
+                    { 103, "1D", null },
+                    { 104, "2A", null },
+                    { 105, "2B", null },
+                    { 106, "2C", null },
+                    { 107, "3A", null },
+                    { 108, "3B", null },
+                    { 109, "3C", null },
+                    { 110, "3S", null },
+                    { 111, "4A", null },
+                    { 112, "4B", null }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "SchoolClasses",
+                keyColumn: "Id",
+                keyValue: 100);
+
+            migrationBuilder.DeleteData(
+                table: "SchoolClasses",
+                keyColumn: "Id",
+                keyValue: 101);
+
+            migrationBuilder.DeleteData(
+                table: "SchoolClasses",
+                keyColumn: "Id",
+                keyValue: 102);
+
+            migrationBuilder.DeleteData(
+                table: "SchoolClasses",
+                keyColumn: "Id",
+                keyValue: 103);
+
+            migrationBuilder.DeleteData(
+                table: "SchoolClasses",
+                keyColumn: "Id",
+                keyValue: 104);
+
+            migrationBuilder.DeleteData(
+                table: "SchoolClasses",
+                keyColumn: "Id",
+                keyValue: 105);
+
+            migrationBuilder.DeleteData(
+                table: "SchoolClasses",
+                keyColumn: "Id",
+                keyValue: 106);
+
+            migrationBuilder.DeleteData(
+                table: "SchoolClasses",
+                keyColumn: "Id",
+                keyValue: 107);
+
+            migrationBuilder.DeleteData(
+                table: "SchoolClasses",
+                keyColumn: "Id",
+                keyValue: 108);
+
+            migrationBuilder.DeleteData(
+                table: "SchoolClasses",
+                keyColumn: "Id",
+                keyValue: 109);
+
+            migrationBuilder.DeleteData(
+                table: "SchoolClasses",
+                keyColumn: "Id",
+                keyValue: 110);
+
+            migrationBuilder.DeleteData(
+                table: "SchoolClasses",
+                keyColumn: "Id",
+                keyValue: 111);
+
+            migrationBuilder.DeleteData(
+                table: "SchoolClasses",
+                keyColumn: "Id",
+                keyValue: 112);
+        }
+    }
+}
